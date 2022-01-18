ab2dcddb660da8bfaf4814766cfa8f7c61dc1d29
Arindam Das <dasarindam.mails@gmail.com>
2022-01-18 11:42:52 +0530

    Set cmdheight to 2

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,7 @@
 -- :help options
 vim.opt.backup = false                          -- creates a backup file
 vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+-- vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
@@ -20,18 +20,16 @@ vim.opt.termguicolors = true                    -- set term gui colors (most ter
 vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.undofile = true                         -- enable persistent undo
 vim.opt.updatetime = 300                        -- faster completion (4000ms default)
-vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to
-						-- file while editing with another program), it is not allowed to
-						-- be edited
+vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = false                       -- *dont't* convert tabs to spaces
 vim.opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 4                             -- insert 2 spaces for a tab
 vim.opt.cursorline = true                       -- highlight the current line
+vim.opt.cursorlineopt = "number"
 vim.opt.number = true                           -- set numbered lines
 vim.opt.relativenumber = false                  -- set relative numbered lines
 vim.opt.numberwidth = 2                         -- set number column width to 2 {default 4}
-vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text
-						-- each time
+vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift text each time
 vim.opt.wrap = false                            -- display lines as one long line
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
@@ -44,6 +42,8 @@ vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 
 vim.cmd [[set fcs=eob:\ ]]
-vim.cmd [[autocmd TermOpen * setlocal nonumber norelativenumber]]  -- hide line numbers in terminal mode
+vim.cmd [[autocmd TermOpen * setlocal nonumber norelativenumber ]]  -- hide line numbers in terminal mode
+vim.cmd [[autocmd TermOpen * setlocal signcolumn=no ]]  -- hide signed column in terminal
+
 
 -- vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
