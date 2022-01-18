9556c8ee0ef2a9c1bf19b51c1a47fc2a5299664a
Arindam Das <dasarindam.mails@gmail.com>
2022-01-18 21:23:53 +0530

    Configured material dark color scheme.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,9 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
+  -- If you are using Packer
+  use 'marko-cerovac/material.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
