c9338fc9eea96188d8a999789834d3bad51e7801
Arindam Das <dasarindam.mails@gmail.com>
2022-01-20 23:35:35 +0530

    Added auto pairs plugin for parenthesis/brackets completion.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -105,6 +105,9 @@ return packer.startup(function(use)
       require("colorizer").setup()
     end
   }
+
+  use "jiangmiao/auto-pairs"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
