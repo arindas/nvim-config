963987b5226e4fcd084b2841f4b95b9d6e2cd40c
Arindam Das <dasarindam.mails@gmail.com>
2022-01-20 18:48:40 +0530

    Added lsp related packages

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,6 +54,8 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
@@ -82,6 +84,10 @@ return packer.startup(function(use)
       require('gitsigns').setup()
     end
   } 
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
