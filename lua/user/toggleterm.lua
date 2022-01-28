147a1395a1d9cc0eafb68ec1c9c4fdc8f9bbdee9
Arindam Das <dasarindam.mails@gmail.com>
2022-01-28 19:53:33 +0530

    Updated toggleterm settings.

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -3,28 +3,27 @@ if not ok then
 	return
 end
 
-toggleterm.setup{
-  -- size can be a number or function which is passed the current terminal
-  size = function(term)
-    if term.direction == "horizontal" then
-      return 15
-    elseif term.direction == "vertical" then
-      return vim.o.columns * 0.4
-    end
-  end,
-  open_mapping = [[<c-\>]],
-  -- on_open = fun(t: Terminal), -- function to run when the terminal opens
-  -- on_close = fun(t: Terminal), -- function to run when the terminal closes
-  hide_numbers = true, -- hide the number column in toggleterm buffers
-  shade_filetypes = {},
-  shade_terminals = true,
-  shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-  start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  persist_size = true,
-  direction = 'horizontal',
-  close_on_exit = true, -- close the terminal window when the process exits
-  shell = vim.o.shell, -- change the default shell
-  -- This field is only relevant if direction is set to 'float'
-
-}
+toggleterm.setup({
+	-- size can be a number or function which is passed the current terminal
+	size = function(term)
+		if term.direction == "horizontal" then
+			return 15
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.4
+		end
+	end,
+	open_mapping = [[<c-\>]],
+	-- on_open = fun(t: Terminal), -- function to run when the terminal opens
+	-- on_close = fun(t: Terminal), -- function to run when the terminal closes
+	hide_numbers = true, -- hide the number column in toggleterm buffers
+	shade_filetypes = {},
+	shade_terminals = false,
+	-- shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+	start_in_insert = true,
+	insert_mappings = true, -- whether or not the open mapping applies in insert mode
+	persist_size = true,
+	direction = "horizontal",
+	close_on_exit = true, -- close the terminal window when the process exits
+	shell = vim.o.shell, -- change the default shell
+	-- This field is only relevant if direction is set to 'float'
+})
