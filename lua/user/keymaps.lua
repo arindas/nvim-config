06d4275f99a752c9fe7fd1254d097a200213c22a
Arindam Das <dasarindam.mails@gmail.com>
2022-01-28 22:54:15 +0530

    Added keymap for disabling search highlight until next search

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -25,6 +25,8 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- Disable search highlight until next search
+keymap("n", "<esc><esc>", ":noh<CR>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
