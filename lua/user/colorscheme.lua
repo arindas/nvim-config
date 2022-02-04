216ac17c7642dc64a2bd28f0fb85b15433b55030
Arindam Das <dasarindam.mails@gmail.com>
2022-02-05 00:54:12 +0530

    Revert to old Telescope look.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -4,12 +4,23 @@ local colorscheme = "base16-material-darker"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	return
 end
 
-vim.cmd [[hi VertSplit guifg=#353535]]
-vim.cmd [[hi NvimTreeNormal guibg=#1a1a1a]]
+vim.cmd([[hi VertSplit guifg=#353535]])
+vim.cmd([[hi NvimTreeNormal guibg=#1a1a1a]])
 
-vim.cmd [[hi LineNr guifg=#4a4a4a]]
-vim.cmd [[hi CursorLineNr guibg=bg]]
+vim.cmd([[hi LineNr guifg=#4a4a4a]])
+vim.cmd([[hi CursorLineNr guibg=bg]])
+
+vim.cmd([[highlight! link TelescopeSelection    Visual]])
+vim.cmd([[highlight! link TelescopeNormal       Normal]])
+vim.cmd([[highlight! link TelescopePromptNormal TelescopeNormal]])
+vim.cmd([[highlight! link TelescopeBorder       TelescopeNormal]])
+vim.cmd([[highlight! link TelescopePromptBorder TelescopeBorder]])
+vim.cmd([[highlight! link TelescopeTitle        TelescopeBorder]])
+vim.cmd([[highlight! link TelescopePromptTitle  TelescopeTitle]])
+vim.cmd([[highlight! link TelescopeResultsTitle TelescopeTitle]])
+vim.cmd([[highlight! link TelescopePreviewTitle TelescopeTitle]])
+vim.cmd([[highlight! link TelescopePromptPrefix Identifier]])
