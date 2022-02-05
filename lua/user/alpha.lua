c2fcbf725fae5f9ebee2b04f9eb5b4756460db96
Arindam Das <dasarindam.mails@gmail.com>
2022-02-05 17:27:41 +0530

    Added browse files shortcut to dashboard.

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -1,24 +1,25 @@
 local syntax_ok, alpha = pcall(require, "alpha")
 if not syntax_ok then
-    return
+	return
 end
 
 local ok, dashboard = pcall(require, "alpha.themes.dashboard")
 if not ok then
-    return
+	return
 end
 
 dashboard.section.buttons.val = {
-    dashboard.button("e", "  New file", "<cmd>ene <CR>"),
-    dashboard.button("SPC f", "  Find file"),
-    dashboard.button("SPC h", "  Recently opened files"),
-    dashboard.button("SPC g", "  Find word"),
-    dashboard.button("q", "  Quit", "<cmd>qa<CR>")
+	dashboard.button("e", "  New file", "<cmd>ene <CR>"),
+	dashboard.button("SPC f", "  Find file"),
+	dashboard.button("SPC /", "  Browse files"),
+	dashboard.button("SPC h", "  Recently opened files"),
+	dashboard.button("SPC g", "  Find word"),
+	dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
 }
 
 local fortune_ok, fortune = pcall(require, "alpha.fortune")
 if fortune_ok then
-    dashboard.section.footer.val = fortune()
+	dashboard.section.footer.val = fortune()
 end
 
 alpha.setup(dashboard.config)
