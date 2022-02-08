4a39d408a89422789126b0da816fee071861068a
Arindam Das <dasarindam.mails@gmail.com>
2022-02-09 00:39:07 +0530

    Removed symbols outline.

--- lua/user/statusline.lua
+++ lua/user/statusline.lua
@@ -34,5 +34,5 @@ lualine_module.setup({
 })
 
 vim.cmd(
-	[[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if &buftype == "nofile" | set laststatus=0 | else | set laststatus=2 | endif]]
+	[[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname("%") == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif]]
 )
