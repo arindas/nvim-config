3aa86947a80b30fabc4a75959ace7487525fce5c
Arindam Das <dasarindam.mails@gmail.com>
2022-02-08 10:37:17 +0530

    Added filetype.nvim for speedup

--- init.lua
+++ init.lua
@@ -1,20 +1,21 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.null_ls"
-require "user.telescope"
-require "user.statusline"
-require "user.bufferline"
-require "user.filetree"
-require "user.treesitter"
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.filetype")
+require("user.colorscheme")
+require("user.cmp")
+require("user.lsp")
+require("user.null_ls")
+require("user.telescope")
+require("user.statusline")
+require("user.bufferline")
+require("user.filetree")
+require("user.treesitter")
 -- require "user.indentline"
-require "user.markdown"
-require "user.toggleterm"
-require "user.impatient"
-require "user.alpha"
+require("user.markdown")
+require("user.toggleterm")
+require("user.impatient")
+require("user.alpha")
 
 -- rust tools
-require "user.rust-tools"
+require("user.rust-tools")
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -40,13 +40,15 @@ packer.init({
 
 -- Install your plugins here
 return packer.startup(function(use)
-	use("lewis6991/impatient.nvim")
-
 	-- essential plugins
 	use("wbthomason/packer.nvim") -- Have packer manage itself
 	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 	use("mfussenegger/nvim-dap")
+
+	-- optimisation
+	use("lewis6991/impatient.nvim")
+	use("nathom/filetype.nvim")
 
 	-- colorscheme
 	use("RRethy/nvim-base16")
