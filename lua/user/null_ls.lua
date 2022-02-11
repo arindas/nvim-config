ffbc76bc0ab54c47931e99e424dfdacf974eb379
Arindam Das <dasarindam.mails@gmail.com>
2022-02-11 21:30:04 +0530

    Updated null_ls config for proper autoformatting

--- lua/user/null_ls.lua
+++ lua/user/null_ls.lua
@@ -4,27 +4,27 @@ if not ok then
 end
 
 local sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.completion.spell,
-    -- null_ls.builtins.formatting.rustfmt,
-    null_ls.builtins.formatting.gofmt,
-    null_ls.builtins.formatting.goimports,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.clang_format,
+	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.diagnostics.eslint,
+	null_ls.builtins.completion.spell,
+	null_ls.builtins.formatting.rustfmt,
+	null_ls.builtins.formatting.gofmt,
+	null_ls.builtins.formatting.goimports,
+	null_ls.builtins.formatting.black,
+	null_ls.builtins.formatting.clang_format,
 }
 
 null_ls.setup({
-    sources = sources,
-    on_attach = function(client)
-    	-- code
-	if client.resolved_capabilities.document_formatting then
-	    vim.cmd([[
+	sources = sources,
+	on_attach = function(client)
+		-- code
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd([[
 	    augroup LspFormatting
 	        autocmd! * <buffer>
-		autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+		autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()
 	    augroup END
 	    ]])
-	end
-    end
+		end
+	end,
 })
