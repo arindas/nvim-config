a6e8f0311b780c2ad4895a0dab25dee868486d53
Arindam Das <dasarindam.mails@gmail.com>
2022-02-27 11:38:39 +0530

    Removed spell as an lsp source, added clangd configuration.

--- lua/user/null_ls.lua
+++ lua/user/null_ls.lua
@@ -6,7 +6,7 @@ end
 local sources = {
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.diagnostics.eslint,
-	null_ls.builtins.completion.spell,
+	-- null_ls.builtins.completion.spell,
 	null_ls.builtins.formatting.rustfmt,
 	null_ls.builtins.formatting.gofmt,
 	null_ls.builtins.formatting.goimports,
