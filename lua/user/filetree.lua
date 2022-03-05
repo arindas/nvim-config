19caa211c3e995cf994239fc0e73f487bb8b7fd0
Arindam Das <dasarindam.mails@gmail.com>
2022-03-05 20:59:12 +0530

    Update nvim config to align with new nvim tree config options.

--- lua/user/filetree.lua
+++ lua/user/filetree.lua
@@ -4,13 +4,7 @@ g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
 -- g.nvim_tree_git_hl = git_status
 g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat({ ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" })
-
-g.nvim_tree_window_picker_exclude = {
-	filetype = { "notify", "packer", "qf" },
-	buftype = { "terminal" },
-}
 
 g.nvim_tree_show_icons = {
 	folders = 1,
@@ -105,5 +99,16 @@ nvim_tree_module.setup({
 	trash = {
 		cmd = "trash",
 		require_confirm = true,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = false,
+			window_picker = {
+				exclude = {
+					filetype = { "notify", "packer", "qf" },
+					buftype = { "terminal" },
+				},
+			},
+		},
 	},
 })
