1b982dbd7bebe2a4728b96b12a7c9991b2580206
Arindam Das <dasarindam.mails@gmail.com>
2022-05-10 17:16:12 +0530

    Set a seperate keybind for setting rust analyzer inlay hints

--- lua/user/rust-tools.lua
+++ lua/user/rust-tools.lua
@@ -10,6 +10,9 @@ local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
+-- enable key hints at start
+
+keymap("n", "<leader>ri", "<cmd>lua pcall(require('rust-tools.inlay_hints').set_inlay_hints)<CR>", opts)
 keymap("n", "<leader>rh", "<cmd>lua pcall(require('rust-tools.inlay_hints').toggle_inlay_hints)<CR>", opts)
 keymap("n", "<leader>rr", "<cmd>lua pcall(require('rust-tools.runnables').runnables)<CR>", opts)
 keymap("n", "<leader>re", "<cmd>lua pcall(require'rust-tools.expand_macro'.expand_macro)<CR>", opts)
