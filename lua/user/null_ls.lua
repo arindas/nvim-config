b014cb56ea8d017829c59fdce74c9a28c1ecf2a6
Arindam Das <dasarindam.mails@gmail.com>
2022-07-11 12:45:44 +0530

    Added shellcheck diagnostics with null-ls

--- lua/user/null_ls.lua
+++ lua/user/null_ls.lua
@@ -6,6 +6,8 @@ end
 local sources = {
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.diagnostics.eslint,
+	null_ls.builtins.diagnostics.shellcheck,
+	null_ls.builtins.formatting.shfmt,
 	-- null_ls.builtins.completion.spell,
 	null_ls.builtins.formatting.rustfmt,
 	null_ls.builtins.formatting.gofmt,
@@ -14,17 +16,28 @@ local sources = {
 	null_ls.builtins.formatting.clang_format,
 }
 
-null_ls.setup({
-	sources = sources,
-	on_attach = function(client)
-		-- code
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd([[
+local function lsp_keymaps(bufnr)
+	local opts = { noremap = true, silent = true }
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+end
+
+local function on_attach(client, bufnr)
+	lsp_keymaps(bufnr)
+
+	-- code
+	if client.resolved_capabilities.document_formatting then
+		vim.cmd([[
 	    augroup LspFormatting
 	        autocmd! * <buffer>
 		autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()
 	    augroup END
 	    ]])
-		end
-	end,
+	end
+end
+
+null_ls.setup({
+	sources = sources,
+	on_attach = on_attach,
 })
