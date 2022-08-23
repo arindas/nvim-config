a1b72071e0b5edd480d7db7bc239b48e6109993c
Arindam Das <dasarindam.mails@gmail.com>
2022-08-23 21:50:48 +0530

    Stick to specific commit for symbols outline.

--- lua/user/symbols_outline.lua
+++ lua/user/symbols_outline.lua
@@ -1,11 +1,5 @@
-local status_ok, symbols_outline = pcall(require, "symbols-outline")
-if not status_ok then
-	vim.notify("symbols outline not found!")
-	return
-end
-
-symbols_outline.setup({
+vim.g.symbols_outline = {
 	preview_bg_highlight = "bg",
 	highlight_hovered_item = false,
 	auto_preview = false,
-})
+}
