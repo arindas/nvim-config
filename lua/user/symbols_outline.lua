29fc15b1c579260b86a1af8f234c4f1d553643a0
Arindam Das <dasarindam.mails@gmail.com>
2022-08-23 19:43:29 +0530

    Use symbols-outline setup() for config instead of global variables.

--- lua/user/symbols_outline.lua
+++ lua/user/symbols_outline.lua
@@ -1,5 +1,11 @@
-vim.g.symbols_outline = {
+local status_ok, symbols_outline = pcall(require, "symbols-outline")
+if not status_ok then
+	vim.notify("symbols outline not found!")
+	return
+end
+
+symbols_outline.setup({
 	preview_bg_highlight = "bg",
 	highlight_hovered_item = false,
 	auto_preview = false,
-}
+})
