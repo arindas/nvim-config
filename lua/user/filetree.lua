20c2eb9c0564feb796f459787a3cfe2c11736cda
Arindam Das <dasarindam.mails@gmail.com>
2022-10-19 20:13:13 +0530

    Updated plugins, removed deprecated parameters

--- lua/user/filetree.lua
+++ lua/user/filetree.lua
@@ -1,111 +1,110 @@
 local setup_ok, nvim_tree_module = pcall(require, "nvim-tree")
 if not setup_ok then
-	return
+    return
 end
 
 nvim_tree_module.setup({
-	disable_netrw = true,
-	hijack_netrw = true,
-	open_on_setup = false,
-	ignore_ft_on_setup = {},
-	open_on_tab = false,
-	hijack_cursor = true,
-	update_cwd = false,
-	diagnostics = {
-		enable = false,
-		icons = {
-			hint = "",
-			info = "",
-			warning = "",
-			error = "",
-		},
-	},
-	update_focused_file = {
-		enable = false,
-		update_cwd = false,
-		ignore_list = {},
-	},
-	system_open = {
-		cmd = nil,
-		args = {},
-	},
-	filters = {
-		dotfiles = false,
-		custom = {},
-	},
-	git = {
-		enable = true,
-		ignore = false,
-		timeout = 500,
-	},
-	view = {
-		width = 30,
-		height = 30,
-		hide_root_folder = false,
-		side = "left",
-		mappings = {
-			custom_only = false,
-			list = {},
-		},
-		number = false,
-		relativenumber = false,
-		signcolumn = "yes",
-	},
-	trash = {
-		cmd = "trash",
-		require_confirm = true,
-	},
-	actions = {
-		open_file = {
-			quit_on_open = false,
-			resize_window = true,
-			window_picker = {
-				exclude = {
-					filetype = { "notify", "packer", "qf" },
-					buftype = { "terminal" },
-				},
-			},
-		},
-	},
-	renderer = {
-		root_folder_modifier = table.concat({
-			":t:gs?$?/..",
-			string.rep(" ", 1000),
-			"?:gs?^??",
-		}),
-		indent_markers = {
-			enable = true,
-		},
-		add_trailing = false,
-		highlight_opened_files = "none",
-		icons = {
-			show = {
-				folder = true,
-				file = true,
-				git = true,
-				folder_arrow = false,
-			},
-			glyphs = {
-				default = "",
-				symlink = "",
-				git = {
-					deleted = "",
-					ignored = "◌",
-					renamed = "➜",
-					staged = "✓",
-					unmerged = "",
-					unstaged = "✗",
-					untracked = "*",
-				},
-				folder = {
-					default = "",
-					empty = "",
-					empty_open = "",
-					open = "",
-					symlink = "",
-					symlink_open = "",
-				},
-			},
-		},
-	},
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_setup = false,
+    ignore_ft_on_setup = {},
+    open_on_tab = false,
+    hijack_cursor = true,
+    update_cwd = false,
+    diagnostics = {
+        enable = false,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
+    update_focused_file = {
+        enable = false,
+        update_cwd = false,
+        ignore_list = {},
+    },
+    system_open = {
+        cmd = nil,
+        args = {},
+    },
+    filters = {
+        dotfiles = false,
+        custom = {},
+    },
+    git = {
+        enable = true,
+        ignore = false,
+        timeout = 500,
+    },
+    view = {
+        width = 30,
+        hide_root_folder = false,
+        side = "left",
+        mappings = {
+            custom_only = false,
+            list = {},
+        },
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
+    },
+    trash = {
+        cmd = "trash",
+        require_confirm = true,
+    },
+    actions = {
+        open_file = {
+            quit_on_open = false,
+            resize_window = true,
+            window_picker = {
+                exclude = {
+                    filetype = { "notify", "packer", "qf" },
+                    buftype = { "terminal" },
+                },
+            },
+        },
+    },
+    renderer = {
+        root_folder_modifier = table.concat({
+            ":t:gs?$?/..",
+            string.rep(" ", 1000),
+            "?:gs?^??",
+        }),
+        indent_markers = {
+            enable = true,
+        },
+        add_trailing = false,
+        highlight_opened_files = "none",
+        icons = {
+            show = {
+                folder = true,
+                file = true,
+                git = true,
+                folder_arrow = false,
+            },
+            glyphs = {
+                default = "",
+                symlink = "",
+                git = {
+                    deleted = "",
+                    ignored = "◌",
+                    renamed = "➜",
+                    staged = "✓",
+                    unmerged = "",
+                    unstaged = "✗",
+                    untracked = "*",
+                },
+                folder = {
+                    default = "",
+                    empty = "",
+                    empty_open = "",
+                    open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+            },
+        },
+    },
 })
--- lua/user/null_ls.lua
+++ lua/user/null_ls.lua
@@ -1,43 +1,43 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-	return
+    return
 end
 
 local sources = {
-	null_ls.builtins.formatting.stylua,
-	null_ls.builtins.diagnostics.eslint,
-	null_ls.builtins.diagnostics.shellcheck,
-	null_ls.builtins.formatting.shfmt,
-	-- null_ls.builtins.completion.spell,
-	null_ls.builtins.formatting.rustfmt,
-	null_ls.builtins.formatting.gofmt,
-	null_ls.builtins.formatting.goimports,
-	null_ls.builtins.formatting.black,
-	null_ls.builtins.formatting.clang_format,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.formatting.shfmt,
+    -- null_ls.builtins.completion.spell,
+    null_ls.builtins.formatting.rustfmt,
+    null_ls.builtins.formatting.gofmt,
+    null_ls.builtins.formatting.goimports,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.clang_format,
 }
 
 local function lsp_keymaps(bufnr)
-	local opts = { noremap = true, silent = true }
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
 end
 
 local function on_attach(client, bufnr)
-	lsp_keymaps(bufnr)
+    lsp_keymaps(bufnr)
 
-	-- code
-	if client.resolved_capabilities.document_formatting then
-		vim.cmd([[
+    -- code
+    if client.server_capabilities.documentFormattingProvider then
+        vim.cmd([[
 	    augroup LspFormatting
 	        autocmd! * <buffer>
-		autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()
+		autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
 	    augroup END
-	    ]])
-	end
+	    ]]  )
+    end
 end
 
 null_ls.setup({
-	sources = sources,
-	on_attach = on_attach,
+    sources = sources,
+    on_attach = on_attach,
 })
