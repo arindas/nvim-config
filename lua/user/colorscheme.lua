a795291707146c6782c645766d881c6f6d4c6f15
Arindam Das <dasarindam.mails@gmail.com>
2023-05-20 14:21:08 +0530

    feat: adds NvimTreeNormaFloat highlight color config

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -14,6 +14,7 @@ vim.cmd([[hi VertSplit guifg=#353535]])
 vim.cmd([[hi TSComment guifg=#6a6a6a]])
 
 vim.cmd([[hi NvimTreeNormal guibg=#1a1a1a]])
+vim.cmd([[hi NvimTreeNormalFloat guibg=#1a1a1a]])
 vim.cmd([[hi NvimTreeWinSeparator guifg=bg]])
 
 vim.cmd([[hi LineNr guifg=#4a4a4a]])
@@ -34,8 +35,12 @@ vim.cmd([[highlight! LspInfoBorder guifg=#ffffff]])
 
 vim.cmd([[hi FocusedSymbol guifg=fg guibg=#353535]])
 
-vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint',
-    numhl = 'DapBreakpoint' })
+vim.fn.sign_define('DapBreakpoint', {
+    text = '',
+    texthl = 'DapBreakpoint',
+    linehl = 'DapBreakpoint',
+    numhl = 'DapBreakpoint'
+})
 vim.fn.sign_define('DapBreakpointCondition',
     { text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
 vim.fn.sign_define('DapBreakpointRejected',
