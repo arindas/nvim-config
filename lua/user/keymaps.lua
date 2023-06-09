38378839cdbdc592e297bc45eeb900518bb93ef6
Arindam Das <dasarindam.mails@gmail.com>
2023-06-09 19:21:16 +0530

    feat: changes keybinds for BufferLineClose<Left|Right>

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -37,8 +37,8 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
-keymap("n", "<A-h>", "<cmd>BufferLineCloseLeft<cr>", opts)
-keymap("n", "<A-l>", "<cmd>BufferLineCloseRight<cr>", opts)
+keymap("n", "<S-A-h>", "<cmd>BufferLineCloseLeft<cr>", opts)
+keymap("n", "<S-A-l>", "<cmd>BufferLineCloseRight<cr>", opts)
 
 -- Insert --
 -- Press jk fast to enter
