f9170a047190f99a9e842f408a3fc91f56b7f36d
Arindam Das <dasarindam.mails@gmail.com>
2024-01-30 17:18:48 +0530

    chore: corrects icons

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -10,12 +10,12 @@ end
 
 dashboard.section.buttons.val = {
     dashboard.button("e", "  New file", "<cmd>ene <CR>"),
-    dashboard.button("SPC t f", "  Find file"),
+    dashboard.button("SPC t f", "󰍉  Find file"),
     dashboard.button("SPC t /", "  Browse files"),
-    dashboard.button("SPC t h", "  Recently opened files"),
-    dashboard.button("SPC t g", "  Find word"),
+    dashboard.button("SPC t h", "󰈢  Recently opened files"),
+    dashboard.button("SPC t g", "󰱼  Find word"),
     dashboard.button("SPC t d", "  Git Diff Preview"),
-    dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+    dashboard.button("q", "󰿅  Quit", "<cmd>qa<CR>"),
 }
 
 local fortune_ok, fortune = pcall(require, "alpha.fortune")
