2b6c5511db66d20feebd905297de7ffbf8966b22
Arindam Das <dasarindam.mails@gmail.com>
2024-07-19 20:47:01 +0530

    feat: override git icons

--- lua/user/icons.lua
+++ lua/user/icons.lua
@@ -15,5 +15,15 @@ require("nvim-web-devicons").setup({
             color = "#FFFFFF",
             name = "Makefile",
         },
+        [".gitignore"] = {
+            icon = "",
+            color = "#f54d27",
+            name = "GitIgnore",
+        },
+        [".gitattributes"] = {
+            icon = "",
+            color = "#f54d27",
+            name = "GitAttributes",
+        },
     },
 })
