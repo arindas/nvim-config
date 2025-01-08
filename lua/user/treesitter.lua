271d7b29e6862a899b9256dce96232ff9a2ba013
Arindam Das <dasarindam.mails@gmail.com>
2025-01-08 13:46:37 +0530

    feat: comment out treesitter ensure_installed

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,18 +1,18 @@
 local ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if not ok then
-	return
+    return
 end
 
 treesitter.setup({
-	ensure_installed = "all",
+    -- ensure_installed = "all",
 
-	sync_install = false,
+    sync_install = false,
 
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = false,
-	},
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = false,
+    },
 })
