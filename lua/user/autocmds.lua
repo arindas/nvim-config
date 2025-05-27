1247e0fbc5fb427756d25c366bf0b2a0cac8f406
Arindam Das <dasarindam.mails@gmail.com>
2025-05-27 12:27:51 +0530

    feat: trigger clipboard sync only when nvim window focus changes

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -2,3 +2,15 @@ vim.cmd([[autocmd TermOpen * setlocal nonumber norelativenumber ]]) -- hide line
 vim.cmd([[autocmd TermOpen * setlocal signcolumn=no ]]) -- hide signed column in terminal
 
 vim.cmd([[au BufRead,BufNewFile *.env set filetype=env syntax=sh]])
+
+-- sync with system clipboard on focus
+vim.api.nvim_create_autocmd({ "FocusGained" }, {
+    pattern = { "*" },
+    command = [[call setreg("@", getreg("+"))]],
+})
+
+-- sync with system clipboard on focus
+vim.api.nvim_create_autocmd({ "FocusLost" }, {
+    pattern = { "*" },
+    command = [[call setreg("+", getreg("@"))]],
+})
